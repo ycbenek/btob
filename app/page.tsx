7a5770ca7499c56fa7d593a95d1804024c0fdf79
bilanco <yigitcanbenekk@gmail.com>
2025-11-29 08:24:49 +0300

    changed

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ export default async function Home() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t container mx-auto">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          © 2024 B2B Catalog Inc. Tüm hakları saklıdır.
+          © 2025 Tüm hakları saklıdır.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
